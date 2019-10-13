ddbc2167d8da55ee05581e9dc710e811808096dc
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-12 21:33:24 -0700

    Added a basic enemy and navmesh

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float chaseRange = 5f;
+    [SerializeField] private Transform basePos;
+    
+    private NavMeshAgent _navMeshAgent;
+    private float _distanceToTarget = Mathf.Infinity;
+
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        var targetPosition = target.position;
+        _distanceToTarget = Vector3.Distance(targetPosition, transform.position);
+        _navMeshAgent.SetDestination(_distanceToTarget <= chaseRange ? targetPosition : basePos.position);
+    }
+}
