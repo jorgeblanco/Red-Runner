0c25d0b1cc9d6bf9296a52932bdc3cc93d705092
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-12 23:25:42 -0700

    Added raycast shooting (no damage)

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Camera fpCamera;
+    [SerializeField] private float range = 50f;
+    
+    private bool _isFiring;
+
+    private void Update()
+    {
+        if (_isFiring)
+        {
+            Shoot();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        _isFiring = Input.GetButton("Fire1");
+    }
+
+    private void Shoot()
+    {
+        var fpCameraTransform = fpCamera.transform;
+        if (Physics.Raycast(fpCameraTransform.position, fpCameraTransform.forward, out var hit, range))
+        {
+            Debug.Log(hit.collider.gameObject.name + " was hit");
+        }
+    }
+}
