8263ab935715477f11fb282eb4b714658d45a0fe
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-12 23:06:41 -0700

    Added enemy state, a gun and a reticle

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,12 +4,32 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    [Header("Targets")]
     [SerializeField] private Transform target;
-    [SerializeField] private float chaseRange = 5f;
     [SerializeField] private Transform basePos;
+    [SerializeField] private MeshRenderer indicator;
+    [Header("Materials")]
+    [SerializeField] private Material idleMaterial;
+    [SerializeField] private Material provokedMaterial;
+    [SerializeField] private Material attackingMaterial;
+    [SerializeField] private Material searchingMaterial;
+    [Header("Properties")]
+    [SerializeField] private float chaseRange = 5f;
+    [SerializeField] private float searchTime = 5f;
+    [SerializeField] private float attackRange = 1f;
     
     private NavMeshAgent _navMeshAgent;
     private float _distanceToTarget = Mathf.Infinity;
+    private EnemyState _state;
+    private float _lostTime;
+
+    enum EnemyState
+    {
+        Idle,
+        Provoked,
+        Attacking,
+        Searching
+    }
 
     private void Start()
     {
@@ -18,8 +38,61 @@ public class EnemyAI : MonoBehaviour
 
     private void Update()
     {
-        var targetPosition = target.position;
-        _distanceToTarget = Vector3.Distance(targetPosition, transform.position);
-        _navMeshAgent.SetDestination(_distanceToTarget <= chaseRange ? targetPosition : basePos.position);
+        UpdateState();
+        switch (_state)
+        {
+            case EnemyState.Idle:
+                _navMeshAgent.SetDestination(basePos.position);
+                indicator.material = idleMaterial;
+                break;
+            case EnemyState.Provoked:
+                _navMeshAgent.SetDestination(target.position);
+                indicator.material = provokedMaterial;
+                break;
+            case EnemyState.Attacking:
+                AttackTarget();
+                indicator.material = attackingMaterial;
+                break;
+            case EnemyState.Searching:
+                indicator.material = searchingMaterial;
+                break;
+        }
+    }
+
+    private void UpdateState()
+    {
+        _distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (_distanceToTarget <= attackRange)
+        {
+            _state = EnemyState.Attacking;
+        }
+        else if (_distanceToTarget <= chaseRange)
+        {
+            _state = EnemyState.Provoked;
+        }
+        else if(_distanceToTarget > chaseRange && _state == EnemyState.Provoked)
+        {
+            _state = EnemyState.Searching;
+            _lostTime = Time.time + searchTime;
+        }
+        else if (_distanceToTarget > chaseRange && Time.time > _lostTime)
+        {
+            _state = EnemyState.Idle;
+        }
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("Attacking");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        var position = transform.position;
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(position, chaseRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(position, attackRange);
     }
 }
