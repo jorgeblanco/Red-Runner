7cbbed7ff6eac0fe42eecbe30f6ca97261fd1206
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-19 21:36:51 -0700

    Added a health counter

---
+++ Assets/Scripts/HealthCounter.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class HealthCounter : MonoBehaviour
+{
+    private TextMeshProUGUI _counter;
+
+    private void Awake()
+    {
+        _counter = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void UpdateCounter(int count)
+    {
+        _counter.SetText($"Health: {count}");
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour, IDamageable, IKillable
 {
@@ -8,17 +7,20 @@ public class PlayerHealth : MonoBehaviour, IDamageable, IKillable
     [SerializeField] private int baseHitPoints = 100;
 
     private DeathHandler _deathHandler;
+    private HealthCounter _healthCounter;
 
     private void Start()
     {
         HitPoints = baseHitPoints;
         _deathHandler = GetComponent<DeathHandler>();
+        _healthCounter = FindObjectOfType<HealthCounter>();
     }
 
     public void TakeDamage(int damage)
     {
         // TODO add damage VFX+SFX
         HitPoints -= damage;
+        _healthCounter.UpdateCounter(HitPoints);
         if (HitPoints <= 0)
         {
             Kill();
