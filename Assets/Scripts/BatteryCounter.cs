715e33b62bd49edeb269e16b48b92c7da71e4c5b
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-21 22:05:16 -0700

    Added a battery counter, a way to toggle the flashlight and a better health pickup mesh

---
+++ Assets/Scripts/BatteryCounter.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class BatteryCounter : MonoBehaviour
+{
+    private TextMeshProUGUI _counter;
+
+    private void Awake()
+    {
+        _counter = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void UpdateCounter(int count)
+    {
+        _counter.SetText($"Battery: {count}%");
+    }
+}
--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -7,17 +7,25 @@ public class Flashlight : MonoBehaviour
     [SerializeField] private float baseIntensity = 4f;
     [SerializeField] private float minAngle = 40f;
     [SerializeField] private float maxAngle = 80f;
+    [SerializeField] private float maxCharge = 2f;
     
     private Light _spotLight;
     private float _remainingCharge = 1f;
+    private BatteryCounter _batteryCounter;
+    private bool _isOn;
 
     private void Start()
     {
         _spotLight = GetComponentInChildren<Light>();
+        _batteryCounter = FindObjectOfType<BatteryCounter>();
+        _spotLight.enabled = _isOn;
     }
 
     private void Update()
     {
+        GetInput();
+        if(!_isOn) return;
+        
         _spotLight.intensity = Mathf.Lerp(0, baseIntensity, _remainingCharge);
         _spotLight.spotAngle = Mathf.Lerp(minAngle, maxAngle, _remainingCharge);
         if (_remainingCharge > 0)
@@ -27,18 +35,31 @@ public class Flashlight : MonoBehaviour
         else
         {
             _spotLight.enabled = false;
+            _isOn = false;
             _remainingCharge = 0;
+        }
+        _batteryCounter.UpdateCounter(GetFlashlightCharge());
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetButtonDown("Flashlight"))
+        {
+            _isOn = !_isOn;
+            _spotLight.enabled = _isOn;
         }
     }
 
     public void RechargeFlashlight(float percentage)
     {
         _spotLight.enabled = true;
+        _isOn = true;
         _remainingCharge += percentage / 100;
+        _remainingCharge = Mathf.Clamp(_remainingCharge, 0, maxCharge);
         // TODO: Play SFX
     }
 
-    public int GetFlashlightCharge()
+    private int GetFlashlightCharge()
     {
         return Mathf.RoundToInt(_remainingCharge * 100);
     }
