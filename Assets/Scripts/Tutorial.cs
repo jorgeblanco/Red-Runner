8e231014ee12b555e8cf60dca37736c04049b2d7
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-26 20:16:34 -0700

    Added a tutorial system

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] private string[] textLines;
+    [SerializeField] private bool showOnce;
+    [SerializeField] private bool shouldInterrupt;
+
+    private bool _wasShown;
+    private TutorialText _tutorialText;
+
+    private void Start()
+    {
+        _tutorialText = FindObjectOfType<TutorialText>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerHealth>() == null) return;
+        if(showOnce && _wasShown) return;
+        
+        _tutorialText.SetTutorialTextMulti(textLines, shouldInterrupt);
+        _wasShown = true;
+    }
+}
---
+++ Assets/Scripts/TutorialText.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TutorialText : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Fade time = Tutorial text length * this")]
+    private float fadeTimeMultiplier = 0.25f;
+    
+    private TextMeshProUGUI _text;
+    private CanvasGroup _canvasGroup;
+    private float _fadeTime;
+    private int _currentTextIndex;
+    private readonly Queue<string> _textLines = new Queue<string>();
+
+    private void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _canvasGroup.alpha = 0;
+    }
+
+    public void SetTutorialTextMulti(string[] textLines, bool interrupt = false)
+    {
+        if (interrupt)
+        {
+            _textLines.Clear();
+            _canvasGroup.alpha = 0;
+        }
+        foreach (var line in textLines)
+        {
+            _textLines.Enqueue(line);
+        }
+    }
+
+    private void SetTutorialText(string text)
+    {
+        _text.SetText(text);
+        _canvasGroup.alpha = 1;
+        _fadeTime = text.Length * fadeTimeMultiplier;
+    }
+
+    private void Update()
+    {
+        if (_canvasGroup.alpha <= 0)
+        {
+            if(_textLines.Count <= 0) return;
+            SetTutorialText(_textLines.Dequeue());
+        }
+
+        _canvasGroup.alpha -= 1 / _fadeTime * Time.deltaTime;
+    }
+}
