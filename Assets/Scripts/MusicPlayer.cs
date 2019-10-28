0876e56a8c0d88a38218970e8dac6c3079dced67
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-27 18:24:15 -0700

    Added BGM

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] bgm;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        if (bgm.Length > 0)
+        {
+            _audioSource.clip = bgm[Random.Range(0, bgm.Length)];
+            _audioSource.Play();
+        }
+    }
+}
