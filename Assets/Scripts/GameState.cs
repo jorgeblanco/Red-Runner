737aa20eceb7f76ff5f1e5bae729f00a01618693
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-29 23:51:26 -0700

    Added a home screen

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -11,10 +11,12 @@ public class GameState : MonoBehaviour
     private bool _shouldReload;
     private bool _shouldQuit;
     private FirstPersonController _fpController;
+    private WeaponSwitcher _weaponSwitcher;
 
     private void Start()
     {
         // UpdateScore();
+        _weaponSwitcher = FindObjectOfType<WeaponSwitcher>();
         _fpController = FindObjectOfType<FirstPersonController>();
     }
     
@@ -63,10 +65,34 @@ public class GameState : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
-        FindObjectOfType<WeaponSwitcher>().enabled = true;
+        _weaponSwitcher.enabled = true;
         _fpController.enabled = true;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+    
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+        Time.timeScale = 1;
+        _weaponSwitcher = FindObjectOfType<WeaponSwitcher>();
+        if (_weaponSwitcher != null)
+        {
+            _weaponSwitcher.enabled = true;
+        }
+        _fpController = FindObjectOfType<FirstPersonController>();
+        if (_fpController != null)
+        {
+            _fpController.enabled = true;
+        }
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
+    public void GoHome()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
     }
 
     public void Quit()
