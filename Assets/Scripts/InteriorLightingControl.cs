e58c508a284c89e917edae17fddb7696fc2bdd91
Jorge Blanco <jbsfreelance@gmail.com>
2019-10-29 23:04:22 -0700

    Added an interior/exterior lighting control

---
+++ Assets/Scripts/InteriorLightingControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class InteriorLightingControl : MonoBehaviour
+{
+    [SerializeField] private Light interiorLight;
+    [SerializeField] private Light exteriorLight;
+
+    private void OnTriggerExit(Collider other)
+    {
+        interiorLight.enabled = false;
+        exteriorLight.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        interiorLight.enabled = true;
+        exteriorLight.enabled = false;
+    }
+}
